925b4ba95b601ef0716cdaa101566b69679c3674
discipleofcoffee <kchawla.85@gmail.com>
2015-09-29 13:55:25 -0700

    Create differently formatted csv for sudoku & Open one

---
+++ SudokuInit.py
@@ -0,0 +1,20 @@
+__author__ = 'Kshitij'
+
+"""
+058000000
+006315890
+710000503
+060050300
+582000471
+007080060
+104000036
+073642100
+000000740
+"""
+
+if __name__ == '__main__':
+
+    sudokuPuzzleFile = open('C:\\Users\Kshitij\OneDrive\ProgrammingProjects\Python\Sudoku\SudokuInitPuzzleF2.csv', encoding='utf-8')
+    puzzleString = sudokuPuzzleFile.read()
+
+    pass
