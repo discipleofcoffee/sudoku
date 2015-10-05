ec3cb9ce63c031c524677074934510d30bbe01fe
Kshitij Chawla <kc.qubit@gmail.com>
2015-10-04 18:08:19 -0700

    Add CellContext()

--- SudokuInit.py
+++ SudokuInit.py
@@ -1,5 +1,8 @@
 __author__ = 'Kshitij'
 
+
+import socket
+
 """
 058000000
 006315890
@@ -14,18 +17,70 @@ __author__ = 'Kshitij'
 !!!! I can create a sudoku variable (class) which can do all this.
 """
 
-import socket
 
-def cellPlaceLocator():
-    index = 0
-    cellPlace = {}
-    for rows in range(0,9):
+class CellContext(object):
+
+    @staticmethod
+    def indexOfCell():
+        index = 0
+        cellPlace = {}
+        for rows in range(0,9):
+            for cols in range(0,9):
+                cellPlace[(rows,cols)] = index
+                index += 1
+        return cellPlace
+
+    @staticmethod
+    def rowOfIndex():
+        rowPlace = {}
+        for rows in range(0, 9):
+            rowPlace[rows] = [elem+(rows*9) for elem in list(range(0, 9, 1))]
+        return rowPlace
+
+    @staticmethod
+    def colOfIndex():
+        colPlace = {}
         for cols in range(0,9):
-            cellPlace[(rows,cols)] = index
-            index += 1
-    return cellPlace
+            colPlace[cols] = [elem+cols for elem in list(range(0, 81, 9))]
+        return colPlace
 
-locTranslat = cellPlaceLocator()
+    @staticmethod
+    def boxOfIndex():
+        boxPlace = {}
+        boxCount = 0
+        for rowBlock in range(0,3):
+            for colCount in range(0,9,3):
+                boxTemp = []
+                for rowCount in range(0+3*rowBlock,3+3*rowBlock):
+                    boxTemp.extend(cellRow[rowCount][colCount:colCount+3])
+                boxPlace[boxCount] = set(boxTemp)
+                boxCount += 1
+        return boxPlace
+
+
+
+
+def setPath():
+    """ Sets the path for the puzzle file depending on the system the program is run on.
+    :return: str
+    """
+    if socket.gethostname() == 'HomeDesktop':
+        puzzlePath = 'C://Users/KC/OneDrive/ProgrammingProjects/Python/Sudoku/'
+    else:
+        puzzlePath = 'C://Users/Kshitij/OneDrive/ProgrammingProjects/Python/Sudoku/'
+    return puzzlePath
+
+def getPuzzle(puzzlePath, puzzleFileName = 'SudokuInitPuzzleF2.csv'):
+    """ Converts puzzle file into a string to be parsed
+    :param puzzlePath: (type: str) puzzle file name's path
+    :param puzzleFileName: (type: str) puzzle file's name. Default: 'SudokuInitPuzzleF2.csv'
+    :return: str
+    """
+    sudokuPuzzleFile = puzzlePath
+    sudokuPuzzleFile = open(puzzlePath+puzzleFileName, encoding='utf-8')
+    sudokuString = sudokuPuzzleFile.read()
+    return sudokuString
+
 
 class SudokuPuzzle(object):
     """ Sudoku Puzzle custom data type. Accepts a string and parses into a sudoku puzzle list of dict for solcing and
@@ -37,7 +92,7 @@ class SudokuPuzzle(object):
     def __init__(self, puzzleInputString):
         self.puzzleInputString = puzzleInputString
         self.sudokuTable = []
-        self.puzzleAsListDict = {}
+        self.puzzleAsList = {}
 
     def boxLoc(self, rowNum, colNum):
         """ Determines sub-box location based on row and column number.
@@ -62,11 +117,11 @@ class SudokuPuzzle(object):
 
         return boxRowLoc + boxColLoc
 
-    def makePuzzleAsListDict(self):
+    def makePuzzleAsList(self):
 
 
         sudokuRows = self.puzzleInputString.split('\n')
-        self.puzzleAsListDict = []
+        self.puzzleAsList = []
         for rows in range(0,9):
             for cols in range(0,9):
                 boxLoc = self.boxLoc(rows, cols)
@@ -78,8 +133,8 @@ class SudokuPuzzle(object):
 
                 rowColBox = (rows, cols, boxLoc)
 
-                self.puzzleAsListDict.append({'rowColBox': rowColBox, 'val': val, 'origVal': origVal, 'mutable': mutable})
-        return self.puzzleAsListDict
+                self.puzzleAsList.append({'rowColBox': rowColBox, 'val': val, 'origVal': origVal, 'mutable': mutable, 'lastModded': False})
+        return self.puzzleAsList
 
     def sudokuTabulate(self):
         """ Converts the sudoku puzzle string into a list of lists to mimic a table structure.
@@ -90,76 +145,47 @@ class SudokuPuzzle(object):
             self.sudokuTable.append(list(rowStrings))
         return self.sudokuTable
 
-    def makePuzzleAsString(self, puzzleAsListDict):
+    def getPuzzleAsList(self):
+        return self.puzzleAsList
+
+    def getPuzzle(self):
         """ Converts the puzzle in a grid-puzzle string form.
         """
 
         puzzleAsString = []
-        for element in puzzleAsListDict:
+        for element in self.puzzleAsList:
             puzzleAsString.append(element['val'])
             if element['rowColBox'][1] == 8 and element['rowColBox'][0] != 8:
                 puzzleAsString.append('\n')
 
         return ''.join(puzzleAsString)
 
+    def setValue(self, newVal, elemRow, elemCol):
+        """ setter function to change values in cell safely
+        :param newVal: int, new value to replace the old one.
+        :param elemRow: int, Row number of the cell to be changed
+        :param elemCol: int, Column number of the cell to be changed
+        :return: list of dict
+        """
+
+        currentIdx = cellIndex[(elemRow, elemCol)]
+        if self.puzzleAsList[currentIdx]['mutable']:
+            self.puzzleAsList[currentIdx]['val'] = newVal
+        else:
+            print('Error: Value immutable:', self.puzzleAsList[currentIdx])
 
 
 
 class SudokuSolver(object):
 
-    def __init__(self, sudokuElements):
+    def __init__(self, puzzle):
 
-        elems = sudokuParser.sudokuElem
+        puzzleList = puzzle.getPuzzleAsList()
+        colVals = {}
 
 
-
-    def considerRow(self,rowNum):
+    def analyzeCell(self):
         pass
-
-def setPath():
-    """ Sets the path for the puzzle file depending on the system the program is run on.
-    :return: str
-    """
-    if socket.gethostname() == 'HomeDesktop':
-        puzzlePath = 'C://Users/KC/OneDrive/ProgrammingProjects/Python/Sudoku/'
-    else:
-        puzzlePath = 'C://Users/Kshitij/OneDrive/ProgrammingProjects/Python/Sudoku/'
-    return puzzlePath
-
-def getPuzzle(puzzlePath, puzzleFileName = 'SudokuInitPuzzleF2.csv'):
-    """ Converts puzzle file into a string to be parsed
-    :param puzzlePath: (type: str) puzzle file name's path
-    :param puzzleFileName: (type: str) puzzle file's name. Default: 'SudokuInitPuzzleF2.csv'
-    :return: str
-    """
-    sudokuPuzzleFile = puzzlePath
-    sudokuPuzzleFile = open(puzzlePath+puzzleFileName, encoding='utf-8')
-    sudokuString = sudokuPuzzleFile.read()
-    return sudokuString
-
-def setValue(puzzleAsListDict, newVal, elemRow, elemCol):
-    """ setter function to change values in cell safely
-    :param puzzleAsListDict: list of dict, the puzzle to be solved
-    :param newVal: int, new value to replace the old one.
-    :param elemRow: int, Row number of the cell to be changed
-    :param elemCol: int, Column number of the cell to be changed
-    :return: list of dict
-    """
-
-    currentIdx = locTranslat[(elemRow, elemCol)]
-    if puzzleAsListDict[currentIdx]['mutable']:
-        puzzleAsListDict[currentIdx]['val'] = newVal
-    else:
-        print('Error: Value immutable:', puzzleAsListDict[currentIdx])
-    return puzzleAsListDict
-
-
-
-
-#    puzzleAsListDict[]
-
-
-
 
 if __name__ == '__main__':
 
@@ -171,13 +197,18 @@ if __name__ == '__main__':
 
     """
 
+    cellIndex = CellContext.indexOfCell()
+    cellRow = CellContext.rowOfIndex()
+    cellCol = CellContext.colOfIndex()
+    cellBox = CellContext.boxOfIndex()
+
     puzzlePath = setPath()
     sudokuString = getPuzzle(puzzlePath, puzzleFileName = 'SudokuInitPuzzleF2.csv')
 
     sudokuPuzzle = SudokuPuzzle(sudokuString)
     table = sudokuPuzzle.sudokuTabulate()
-    elems = sudokuPuzzle.makePuzzleAsListDict()
-    puzzleString = sudokuPuzzle.makePuzzleAsString(elems)
+    elems = sudokuPuzzle.makePuzzleAsList()
+    puzzleString = sudokuPuzzle.getPuzzle()
     print(sudokuString == puzzleString)
     print(puzzleString,'\n')
     print(sudokuString)
